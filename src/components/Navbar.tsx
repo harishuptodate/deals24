3a30ce19a5ee912ef54420c24d16548c18e0b9bd
harishuptodate <harishuptodate@gmail.com>
2025-03-23 21:01:11 +0530

    fixed searchbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -98,12 +98,12 @@ const Navbar = () => {
 									<div className="relative">
 										<Input
 											ref={inputRef}
-											type="search"
+											type="text"
 											placeholder="Search deals..."
 											className="w-full pl-10 pr-4 py-2 border rounded-full focus:outline-none focus:ring-2 focus:ring-apple-darkGray"
 											value={searchQuery}
 											onChange={(e) => setSearchQuery(e.target.value)}
-											onFocus={() => setIsSearchPopoverOpen(true)}
+											onClick={() => setIsSearchPopoverOpen(true)}
 										/>
 										<Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
 										<Button
