54990694d5f791d4ba8374927d148c3cf1da2a6b
harishuptodate <harishuptodate@gmail.com>
2025-04-14 01:33:24 +0530

    updated authService

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,5 +1,5 @@
 const isAuthenticated = () => {
-  return localStorage.getItem('adminAuth') === 'true';
+  return localStorage.getItem('adminAuth') === '____________';
 };
 
 const login = (username: string, password: string): boolean => {
@@ -7,7 +7,7 @@ const login = (username: string, password: string): boolean => {
     username === import.meta.env.VITE_ADMIN_USERNAME && 
     password === import.meta.env.VITE_ADMIN_PASSWORD
   ) {
-    localStorage.setItem('adminAuth', 'true');
+    localStorage.setItem('adminAuth', '____________');
     return true;
   }
   return false;
