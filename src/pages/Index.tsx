d237b60c42c39b97b9dc73b58091e9887ad5b028
harishuptodate <harishuptodate@gmail.com>
2025-04-23 20:35:41 +0530

    now spotlight only in dark mode

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,11 +9,13 @@ const Index = () => {
 	return (
 		<div className="min-h-screen bg-gradient-to-b from-apple-lightGray to-white dark:from-[#09090B] dark:to-[#09090B]">
 			{/* Spotlight behind */}
-			<Spotlight
-				className="absolute left-1/4 top-0 -translate-x-1/2 scale-125 md:left-60 md:top-[-5rem] md:scale-100 max-w-full overflow-hidden md:max-w-none"
-				fill="gray"
-			/>
-			<Navbar />
+						<div className="dark:block hidden">
+							<Spotlight
+								className="absolute left-1/4 top-0 -translate-x-1/2 scale-125 md:left-60 md:top-[-5rem] md:scale-100 max-w-full overflow-hidden md:max-w-none"
+								fill="gray"
+							/>
+						</div>
+						<Navbar />
 			<main>
 				<HeroSection />
 				<DealGrid />
