4c845a1e318967d2f7e257c568ba5319eac31036
harishuptodate <harishuptodate@gmail.com>
2025-04-23 15:47:44 +0530

    fixed spotlight overflow

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,8 +7,7 @@ import { Spotlight } from '@/components/ui/Spootlight';
 
 const Index = () => {
 	return (
-		<div className="min-h-screen bg-gradient-to-b from-apple-lightGray to-white dark:from-[#09090B] dark:to-[#09090B]">
-			{/* Spotlight behind */}
+		<div className="relative overflow-hidden min-h-screen bg-gradient-to-b from-apple-lightGray to-white dark:from-[#09090B] dark:to-[#09090B]">
 			<Spotlight
 				className="left-1/2 top-24 -translate-x-1/2 scale-125 md:left-60 md:top-[-5rem] md:scale-100"
 				fill="gray"
