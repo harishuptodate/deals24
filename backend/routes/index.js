b056ed795e0b044b02027ea9b9d5c713ed9fa3fb
harishuptodate <harishuptodate@gmail.com>
2025-06-19 19:11:36 +0530

    added health check route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,5 +5,7 @@ const telegramRoutes = require('./telegram');
 
 // Telegram routes - we'll mount these at /api/telegram in index.js
 router.use('/telegram', telegramRoutes);
-
+router.get('/health', (req, res) => {
+  res.status(200).json('Backend is up and running');
+});
 module.exports = router;
